8f838d3ba7e2df5cf0fb6826897f0550ed50d417
ChanguaLover4ever <danivillacast06@gmail.com>
2026-09-17 22:54:09 -0500

    feat: GREEN - throws exception when rolling after game is complete

--- src/main/java/edu/eci/dosw/bowling/BowlingGame.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingGame.java
@@ -20,6 +20,10 @@ public class BowlingGame {
     /** Registra pinos derribados. Lanza IllegalArgumentException si pines < 0 o > 10.
      *  Lanza IllegalStateException si el juego ya termino. */
     public void roll(int pins) {
+        if (frames.size() == 10 && frames.get(9).isFull()) {
+            throw new IllegalStateException("Cannot roll, the game is complete");
+        }
+
         if (pins < 0 || pins > 10) {
             throw new IllegalArgumentException("The number of pins is not valid");
         }

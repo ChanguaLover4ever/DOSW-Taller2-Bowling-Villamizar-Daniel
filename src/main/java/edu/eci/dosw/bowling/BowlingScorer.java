e1d720d5fb13eca7e137b44e708594e07ae58027
ChanguaLover4ever <danivillacast06@gmail.com>
2026-09-17 23:26:40 -0500

    refactor: extract bonus calculation logic to private methods

--- src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
@@ -4,26 +4,39 @@ import java.util.List;
 
 public class BowlingScorer {
 
-    public int calculate(java.util.List<Frame> frames) {
+    public int calculate(List<Frame> frames) {
         int score = 0;
         for (int i = 0; i < frames.size(); i++) {
             Frame frame = frames.get(i);
             score += frame.getPinsKnockedDown();
 
-            if (frame.getType() == FrameType.SPARE && i + 1 < frames.size()) {
-                score += frames.get(i + 1).getFirstRoll();
-            } else if (frame.getType() == FrameType.STRIKE && i + 1 < frames.size()) {
-                Frame nextFrame = frames.get(i + 1);
-
-                // If the next frame is also a strike, we need the first roll of the frame after that
-                if (nextFrame.getType() == FrameType.STRIKE && i + 2 < frames.size()) {
-                    score += nextFrame.getFirstRoll() + frames.get(i + 2).getFirstRoll();
-                } else {
-                    // Otherwise, the next frame has at least two rolls we can use
-                    score += nextFrame.getFirstRoll() + nextFrame.getSecondRoll();
-                }
+            if (frame.getType() == FrameType.STRIKE) {
+                score += strikeBonus(i, frames);
+            } else if (frame.getType() == FrameType.SPARE) {
+                score += spareBonus(i, frames);
             }
         }
         return score;
+    }
+
+    private int spareBonus(int frameIndex, List<Frame> frames) {
+        if (frameIndex + 1 < frames.size()) {
+            return frames.get(frameIndex + 1).getFirstRoll();
+        }
+        return 0;
+    }
+
+    private int strikeBonus(int frameIndex, List<Frame> frames) {
+        if (frameIndex + 1 >= frames.size()) {
+            return 0;
+        }
+
+        Frame nextFrame = frames.get(frameIndex + 1);
+
+        if (nextFrame.getType() == FrameType.STRIKE && frameIndex + 2 < frames.size()) {
+            return nextFrame.getFirstRoll() + frames.get(frameIndex + 2).getFirstRoll();
+        }
+
+        return nextFrame.getFirstRoll() + nextFrame.getSecondRoll();
     }
 }

7681987161c743eb403d7b4fc1ba5ef8aa27d866
ChanguaLover4ever <danivillacast06@gmail.com>
2026-09-17 23:12:03 -0500

    test: RED - should return zero when all rolls are zero

--- src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
+++ src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -1,4 +1,27 @@
 package edu.eci.dosw.bowling;
 
-public class BowlingScorerTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BowlingScorerTest {
+
+    private void rollMany(BowlingGame game, int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
+    }
+
+    @Test
+    void shouldReturnZero_whenAllRollsAreZero() {
+        // Arrange
+        BowlingGame game = new BowlingGame();
+        rollMany(game, 20, 0);
+        BowlingScorer scorer = new BowlingScorer();
+
+        // Act
+        int score = scorer.calculate(game.getFrames());
+
+        // Assert
+        assertEquals(0, score, "A game with all gutter balls should score 0");
+    }
 }

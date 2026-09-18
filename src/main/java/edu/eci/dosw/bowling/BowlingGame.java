7fa3d3b809c8c510e81b5534fbd6e325da858df3
ChanguaLover4ever <danivillacast06@gmail.com>
2026-09-17 22:08:34 -0500

    feat: GREEN - roll registers a frame

--- src/main/java/edu/eci/dosw/bowling/BowlingGame.java
+++ src/main/java/edu/eci/dosw/bowling/BowlingGame.java
@@ -20,7 +20,8 @@ public class BowlingGame {
     /** Registra pinos derribados. Lanza IllegalArgumentException si pines < 0 o > 10.
      *  Lanza IllegalStateException si el juego ya termino. */
     public void roll(int pins) {
-        // TODO: implementar con TDD (RED -> GREEN -> REFACTOR)
+        Frame frame = new Frame();
+        frames.add(frame);
     }
 
     /** Puntaje total. Lanza IllegalStateException si el juego no esta completo. */
--- src/main/java/edu/eci/dosw/bowling/FrameType.java
+++ src/main/java/edu/eci/dosw/bowling/FrameType.java
@@ -1,4 +1,5 @@
 package edu.eci.dosw.bowling;
 
 public enum FrameType {
+    NORMAL, SPARE, STRIKE, TENTH
 }
